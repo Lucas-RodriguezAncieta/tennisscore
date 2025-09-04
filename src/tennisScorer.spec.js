c27a999c6ca82780e4ffdafdd888e714ffcbea2b
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-09-04 04:17:25 -0400

    2da prueba de player 2 anota 2 puntoS

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -1,12 +1,13 @@
 import { TennisScorer } from "./tennisScorer.js";
 
 describe("TennisScorer", () => {
+  //INICIO
     it("game empieza con ambos en cero", () => {
         const score = new TennisScorer(); 
         const result = score.showScore(); 
         expect(result).toEqual("Love - Love");
     });
-
+    //PRUEBAS PLAYER 1
     it("player 1 anota un punto y el jugador no anota, 1-0", () => {
          const score = new TennisScorer(); 
         score.player1Scores();            
@@ -29,10 +30,18 @@ describe("TennisScorer", () => {
         expect(score.showScore()).toEqual("40 - Love");
     });
     
-
+    //PRUEBAS PARA PLAYER 2
     it("player 2 anota su primer punto y el jugador 2 no anota 0 - 1 ", () => {
         const score = new TennisScorer();
         score.player2Scores();
         expect(score.showScore()).toEqual("Love - 15");
     });
+
+    it("player 2 anota dos puntos y el jugador 2 no anota 0 - 2 ", () => {
+        const score = new TennisScorer();
+        score.player2Scores();
+        score.player2Scores();
+        expect(score.showScore()).toEqual("Love - 30");
+    });
+
 });
