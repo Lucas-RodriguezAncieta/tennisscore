72aa35e94d7e175b819aff0a301fe8b420ae9bf1
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-09-04 04:00:13 -0400

    prueba de tenis que seria los dos en cero

--- src/tennisScorer.js
+++ src/tennisScorer.js
@@ -1,19 +1,25 @@
 class TennisScorer {
-  constructor() {
-    this.player1Points = 0;
-  }
-  showScore() {
-    if (this.player1Points === 1) {
-      return "15 - Love";
-    } else if (this.player1Points === 2) {
-      return "30 - Love";
-    } else if (this.player1Points === 3) {
-      return "40 - Love";
+    constructor() {
+        this.player1Points = 0;
+        this.player2Points = 0;
     }
-    return "Love - Love";
-  }
-  player1Scores() {
-    this.player1Points++;
-  }
+
+    showScore() {
+        if (this.player1Points === 1) {
+            return "15 - Love";
+        } else if (this.player1Points === 2) {
+            return "30 - Love";
+        }
+        return "Love - Love";
+    }
+
+    player1Scores() {
+        this.player1Points++;
+    }
+
+    player2Scores() {
+        this.player2Points++;
+    }
 }
-export default TennisScorer;
+
+export { TennisScorer };
