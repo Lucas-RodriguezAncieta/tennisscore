682566a79245bb4f9aa34aa166870599d05665e0
Lucas-RodriguezAncieta <lucassantiago.lsra@gmail.com>
2025-09-04 04:07:24 -0400

    prueba de tenis el resultado del jugador 1 anota 3 veces resultado tres  a cero

--- src/tennisScorer.spec.js
+++ src/tennisScorer.spec.js
@@ -6,19 +6,26 @@ describe("TennisScorer", () => {
         expect(score.showScore()).toEqual("Love - Love");
     });
 
-    it("player 1 anota un punto y el jugador no anota nada 1-0", () => {
+    it("player 1 anota un punto y el jugador no anota, 1-0", () => {
         const score = new TennisScorer();
         score.player1Scores();
         expect(score.showScore()).toEqual("15 - Love");
     });
 
     
-    it("player 1 anota dos veces y el juegador no anota nada 2-0", () => {
+    it("player 1 anota dos veces y el juegador no anota, 2-0", () => {
         const score = new TennisScorer();
         score.player1Scores();
         score.player1Scores();
         expect(score.showScore()).toEqual("30 - Love");
     });
 
+    it("player 1 anota tres veces y el juegador no anota, 3-0", () => {
+        const score = new TennisScorer();
+        score.player1Scores();
+        score.player1Scores();
+        score.player1Scores();
+        expect(score.showScore()).toEqual("40 - Love");
+    });
     
 });
